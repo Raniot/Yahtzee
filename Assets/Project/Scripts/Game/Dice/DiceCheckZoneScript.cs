c0e7ee43c1aefe5475c70de3eed31755084921db
rasmus hemmingsen <rasmushem93@hotmail.com>
2020-04-20 21:26:00 +0200

    made simple dices throws

---
+++ Assets/Project/Scripts/Game/Dice/DiceCheckZoneScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceCheckZoneScript : MonoBehaviour
+{
+    private Vector3 dice1Velocity;
+    private Vector3 dice2Velocity;
+    private Vector3 dice3Velocity;
+    private Vector3 dice4Velocity;
+    private Vector3 dice5Velocity;
+    private Vector3 dice6Velocity;
+
+    public Text lblDice1;
+    public Text lblDice2;
+    public Text lblDice3;
+    public Text lblDice4;
+    public Text lblDice5;
+    public Text lblDice6;
+
+    // Start is called before the first frame update
+    private void FixedUpdate()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void OnTriggerStay(Collider dice)
+    {
+        //var rb = dice.gameObject.transform.parent.GetComponent<Rigidbody>();
+
+        var rb = 1;
+        if (rb == 1)
+        {
+            switch(dice.gameObject.name)
+            {
+                case "Side1":
+                    SetLabelText(dice.gameObject.transform.parent.name, 1);
+                    break;
+                case "Side2":
+                    SetLabelText(dice.gameObject.transform.parent.name, 2);
+                    break;
+                case "Side3":
+                    SetLabelText(dice.gameObject.transform.parent.name, 3);
+                    break;
+                case "Side4":
+                    SetLabelText(dice.gameObject.transform.parent.name, 4);
+                    break;
+                case "Side5":
+                    SetLabelText(dice.gameObject.transform.parent.name, 5);
+                    break;
+                case "Side6":
+                    SetLabelText(dice.gameObject.transform.parent.name, 6);
+                    break;
+            }
+        }
+    }
+
+    private void SetLabelText(string diceName, int side)
+    {
+        
+        switch(diceName)
+        {
+            case "Dice 1":
+                lblDice1.text = diceName + ": " + side;
+                break;
+            case "Dice 2":
+                lblDice2.text = diceName + ": " + side;
+                break;
+            case "Dice 3":
+                lblDice3.text = diceName + ": " + side;
+                break;
+            case "Dice 4":
+                lblDice4.text = diceName + ": " + side;
+                break;
+            case "Dice 5":
+                lblDice5.text = diceName + ": " + side;
+                break;
+            case "Dice 6":
+                lblDice6.text = diceName + ": " + side;
+                break;
+        }
+    }
+}
---
+++ Assets/Project/Scripts/Game/Dice/DiceScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceScript : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public Vector3 startPos;
+    public Text text;
+    public int diceNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void RollDice()
+    {
+        text.text = "Dice " + diceNumber + ":";
+        float dirX = Random.Range(-1000, 1000);
+        float dirY = Random.Range(-1000, 1000);
+        float dirZ = Random.Range(-1000, 1000);
+        transform.position = new Vector3(Random.Range(-2, 2), Random.Range(2, 4), Random.Range(-2, 2)) ;
+        transform.rotation = new Quaternion(dirX, dirY, dirZ, 1);
+        rb.AddForce(transform.up * Random.Range(500, 800));
+        rb.AddTorque(dirX, dirY, dirZ);
+    }
+}
